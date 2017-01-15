1e266a374240c6f1bef52d6bdac602f2c2787c72
Duc Dao <huuduc.uneti@gmail.com>
2017-01-15 17:11:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,27 +16,4 @@ function __autoload($class){
 
 	$core = new Bootstrap();
 	$core->init();
-
-// require "user/models/category_model.php";
-// $cat = new Category_Model();
-// $str = "<p style='color: #07c;'>paragraph</p>";
-// $str = htmlentities($str);
-// $arg = array(
-// 	"category_name" => "html",
-// 	"description" => $str,
-// 	"date_add" => date("Y-m-d h:i:s"),
-// 	"date_modified" => date("Y-m-d h:i:s")
-// 	);
-
-// $cat->insert_category($arg);
-
-// $data = $cat->get_all_category("SELECT * FROM category ORDER BY date_modified DESC");
-// $list = "";
-// if (isset($data) && !empty($data) && $data != null ){
-// 	foreach ($data as $row) {
-// 		$list .=  "".html_entity_decode($row["description"])."";
-// 	}
-
-// }
-// echo $list;
 ?>
